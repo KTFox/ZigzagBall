a965fccd361451a172a42ddd659cf81f94a350b2
Trần Xuân Khôi <khoitran7122002@gmail.com>
2023-03-26 20:25:22 +0700

    New Update: Add ObjectPooler script.

--- Zigzag/Assets/_Scripts/AudioManager.cs
+++ Zigzag/Assets/_Scripts/AudioManager.cs
@@ -4,6 +4,22 @@ using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+
+        public AudioClip audioClip;
+
+        [Range(0f, 1f)] public float volume;
+        [Range(0.1f, 3f)] public float pitch;
+        public bool loop;
+
+        [HideInInspector] public AudioSource source;
+    }
+
+
+
     //Variables
     public static AudioManager instance;
     public Sound[] sounds;
